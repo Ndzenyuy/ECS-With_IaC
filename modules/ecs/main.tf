d6cc95d558b29993ad0b86ecd7d4c5f3020163a8
Ndzenyuy <ndzenyuyjones@gmail.com>
2025-06-29 04:17:58 +0100

    added all 4 task definitions

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -96,9 +96,9 @@ resource "aws_cloudwatch_log_group" "nginx" {
 
 
 #####Task definitions
-
+#client
 resource "aws_ecs_task_definition" "client" {
-  family                   = "${var.project_name}-api-Task"
+  family                   = "${var.project_name}-client-Task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = var.container_cpu
@@ -107,8 +107,8 @@ resource "aws_ecs_task_definition" "client" {
   task_role_arn            = var.task_role_arn
 
   container_definitions = jsonencode([{
-    name      = "api"
-    image     = var.api_image
+    name      = "client"
+    image     = var.client_image
     essential = true
     portMappings = [{
       containerPort = var.client_container_port
@@ -125,6 +125,99 @@ resource "aws_ecs_task_definition" "client" {
     }
   }])
 }
+
+#api
+resource "aws_ecs_task_definition" "api" {
+  family                   = "${var.project_name}-api-Task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = var.container_cpu
+  memory                   = var.container_memory
+  execution_role_arn       = var.execution_role_arn
+  task_role_arn            = var.task_role_arn
+
+  container_definitions = jsonencode([{
+    name      = "api"
+    image     = var.api_image
+    essential = true
+    portMappings = [{
+      containerPort = var.api_container_port
+      hostPort      = var.api_container_port
+      protocol      = "tcp"
+    }]
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        awslogs-group         = aws_cloudwatch_log_group.api.name
+        awslogs-region        = var.region
+        awslogs-stream-prefix = "ecs"
+      }
+    }
+  }])
+}
+
+#webapi
+resource "aws_ecs_task_definition" "webapi" {
+  family                   = "${var.project_name}-webapi-Task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = var.container_cpu
+  memory                   = var.container_memory
+  execution_role_arn       = var.execution_role_arn
+  task_role_arn            = var.task_role_arn
+
+  container_definitions = jsonencode([{
+    name      = "webapi"
+    image     = var.webapi_image
+    essential = true
+    portMappings = [{
+      containerPort = var.webapi_container_port
+      hostPort      = var.webapi_container_port
+      protocol      = "tcp"
+    }]
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        awslogs-group         = aws_cloudwatch_log_group.webapi.name
+        awslogs-region        = var.region
+        awslogs-stream-prefix = "ecs"
+      }
+    }
+  }])
+}
+
+#api
+resource "aws_ecs_task_definition" "nginx" {
+  family                   = "${var.project_name}-nginx-Task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = var.container_cpu
+  memory                   = var.container_memory
+  execution_role_arn       = var.execution_role_arn
+  task_role_arn            = var.task_role_arn
+
+  container_definitions = jsonencode([{
+    name      = "nginx"
+    image     = var.nginx_image
+    essential = true
+    portMappings = [{
+      containerPort = var.nginx_container_port
+      hostPort      = var.nginx_container_port
+      protocol      = "tcp"
+    }]
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        awslogs-group         = aws_cloudwatch_log_group.nginx.name
+        awslogs-region        = var.region
+        awslogs-stream-prefix = "ecs"
+      }
+    }
+  }])
+}
+
+
+
 
 #### Services
 
@@ -147,3 +240,56 @@ resource "aws_ecs_service" "client" {
   }
 }
 
+resource "aws_ecs_service" "api" {
+  name            = "${var.project_name}-Service-api"
+  cluster         = aws_ecs_cluster.ecs-cluster.id
+  task_definition = aws_ecs_task_definition.api.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = var.subnet_ids
+    security_groups  = [var.security_group_id]
+    assign_public_ip = false
+  }
+
+  service_registries {
+    registry_arn = aws_service_discovery_service.api.arn
+  }
+}
+
+resource "aws_ecs_service" "webapi" {
+  name            = "${var.project_name}-Service-webapi"
+  cluster         = aws_ecs_cluster.ecs-cluster.id
+  task_definition = aws_ecs_task_definition.webapi.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = var.subnet_ids
+    security_groups  = [var.security_group_id]
+    assign_public_ip = false
+  }
+
+  service_registries {
+    registry_arn = aws_service_discovery_service.webapi.arn
+  }
+}
+
+resource "aws_ecs_service" "nginx" {
+  name            = "${var.project_name}-Service-nginx"
+  cluster         = aws_ecs_cluster.ecs-cluster.id
+  task_definition = aws_ecs_task_definition.nginx.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = var.subnet_ids
+    security_groups  = [var.security_group_id]
+    assign_public_ip = false
+  }
+
+  service_registries {
+    registry_arn = aws_service_discovery_service.nginx.arn
+  }
+}
